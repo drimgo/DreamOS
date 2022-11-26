ed744ea3fc6442a0ce59c07a6a42295e181e9c96
drimgo <rserezka@gmail.com>
2022-11-26 17:16:38 +0300

    added reading disc's memory

--- Makefile
+++ Makefile
@@ -3,8 +3,9 @@ LIBS = print.asm
 LIBS_OBJ = print.o
 .PHONY: default compile build run
 default: build run
-compile: $(LIBS)
-	fasm print.asm print.bin
+compile: 
+	
 	fasm boot.asm boot.bin
-build: boot.asm $(LIBS_OBJ)
-	cat boot.bin print.bin > os.bin
+	
+#build: 
+#	cat boot.bin print.bin discloader.bin > os.bin
--- boot.asm
+++ boot.asm
@@ -2,15 +2,28 @@
 use16
 org 0x7c00
 
+
+
 start: 
+    mov bp, 0x8000
+    mov sp, bp
+    
     mov bx, msg
     call print_string
+    mov bx, 0x9000
+    mov dh, 2
+    call disc_load
 
     jmp $
 
-
 include "print.asm"
-msg db "Bootloader start", 0
+include "discloader.asm"
+
+
+msg db "Bootloader start", 0xA, 0
 
 times 510-($-$$) db 0
 dw 0xaa55 
+
+times 256 dw 0xdada					
+times 256 dw 0xface
